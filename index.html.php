c564f915403c18214f281140903ab0a8d2dcf51a
Chris Owens <cowens17@cnm.edu>
2017-11-14 15:27:04 -0800

    added copy text button

--- index.html.php
+++ index.html.php
@@ -20,9 +20,20 @@
 	<body>
 <h1>This is my dope ass JavaScript Example!</h1>
 		<h2>Here's some fuckin' cool stuff!</h2>
+		<div>
+			<h2>watch what happens to the color!</h2>
+			<p id="demo" onclick="myFunction()">Press me!</p>
+		</div>
+		<div>
+			<h3>This shit is semi-useful, it lets you copy what you type! </h3>
+			<input type="text" id="field1" value="Hell Yeah">
+			<input type="text" id="field2">
+			<button onclick="myOtherFunction()">Copy Dat Shit</button>
+		</div>
+		<button id="strap" type="button" class="btn btn-danger">Now try clicking this shit!</button>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	</body>
-	<div>
-		<h2>watch what happens to the color!</h2>
-		<p id="demo" onclick="myFunction()">Press me!</p>
-	</div>
+
 </html>
--- js/events.js
+++ js/events.js
@@ -3,3 +3,12 @@
 function myFunction() {
 	document.getElementById("demo").style.color = "red";
 }
+// this makes a copy from one field to another
+
+function myOtherFunction() {
+	document.getElementById("field2").value = document.getElementById("field1").value;
+}
+document.getElementById("strap").onclick = function () { niceClickFunction() }
+function niceClickFunction() {
+	document.getElementById("strap").innerHTML = "GoodShit!";
+}
